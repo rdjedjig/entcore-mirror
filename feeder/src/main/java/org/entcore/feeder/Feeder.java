simbas/entcore

[Fix][Feeder] Fix Classes Mapping result output

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -336,8 +336,9 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 			@Override
 			public void handle(JsonObject event) {
 				if (!v.containsErrors()) {
-					sendOK(message, new JsonObject()
-							.putObject("classesMapping", v.getClassesMappings()));
+					JsonObject result = new JsonObject().putObject("result", v.getResult());
+					result.getObject("result").removeField("errors");
+					sendOK(message, result);
 				} else {
 					sendError(message, "classes.mapping.error");
 				}

rdjedjig/entcore-mirror

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -344,8 +344,9 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 			@Override
 			public void handle(JsonObject event) {
 				if (!v.containsErrors()) {
-					sendOK(message, new JsonObject()
-							.put("classesMapping", v.getClassesMappings()));
+					JsonObject result = new JsonObject().put("result", v.getResult());
+					result.getJsonObject("result").remove("errors");
+					sendOK(message, result);
 				} else {
 					sendError(message, "classes.mapping.error");
 				}