simbas/entcore

[Feeder] Make missing.attribute and invalid.value softError

feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -619,7 +619,6 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 						}
 						switch (profile) {
 							case "Relative":
-								JsonArray linkStudents = new JsonArray();
 								if ("Intitulé".equals(strings[0]) && "Adresse Organisme".equals(strings[1])) {
 									break csvParserWhile;
 								}
@@ -709,20 +708,24 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 								}
 								break;
 						}
-						String error = validator.validate(user, acceptLanguage, true);
+						JsonArray errorsContext = new JsonArray(); // Must follow that shape : [{"reason":"error.key", "attribute":"lastName", "value":""}...]
+						String error = validator.validate(user, acceptLanguage, true, errorsContext);
 						if (error != null) {
 							log.warn(error);
-							addErrorByFile(profile, "validator.errorWithLine", "" + (i+1), error); // Note that 'error' is already translated
-						} else {
-							final String classesStr = Joiner.on(", ").join(classesNames);
-							classesNamesMapping.put(user.getString("externalId"), classesStr);
-							addUser(profile, user.putString("state", translate(state.name()))
-									.putString("translatedProfile", translate(profile))
-									.putString("classesStr", classesStr)
-									.putArray("childExternalId", linkStudents)
-									.putNumber("line", i + 1)
-							);
+							for (Object ec : errorsContext) {
+								JsonObject err = (JsonObject)ec;
+								addSoftErrorByFile(profile,
+										err.getString("reason"), "" + (i + 1), err.getString("attribute"), err.getString("value"));
+							}
 						}
+						final String classesStr = Joiner.on(", ").join(classesNames);
+						classesNamesMapping.put(user.getString("externalId"), classesStr);
+						addUser(profile, user.putString("state", translate(state.name()))
+										.putString("translatedProfile", translate(profile))
+										.putString("classesStr", classesStr)
+										.putArray("childExternalId", linkStudents)
+										.putNumber("line", i + 1)
+						);
 						i++;
 					}
 				} catch (Exception e) {
@@ -753,16 +756,6 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 				}
 			}
 		});
-	}
-
-	private void relativeStudentMapping(JsonArray linkStudents, String mapping) {
-		if (mapping.trim().isEmpty()) return;
-		try {
-			String hash = Hash.sha1(mapping.getBytes("UTF-8"));
-			linkStudents.add(getOrElse(studentExternalIdMapping.get(hash), hash));
-		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-			log.error(e.getMessage(), e);
-		}
 	}
 
 	public ProfileColumnsMapper getColumnsMapper() {

rdjedjig/entcore-mirror

feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -762,7 +762,8 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 								}
 								break;
 						}
-						String error = validator.validate(user, acceptLanguage, true);
+						JsonArray errorsContext = new JsonArray(); // Must follow that shape : [{"reason":"error.key", "attribute":"lastName", "value":""}...]
+						String error = validator.validate(user, acceptLanguage, true, errorsContext);
 						if (error != null) {
 							log.warn(error);
 							addErrorByFile(profile, "validator.errorWithLine", "" + (i+1), error); // Note that 'error' is already translated
@@ -777,7 +778,20 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 									.put("childExternalId", linkStudents)
 									.put("line", i + 1)
 							);
+							for (Object ec : errorsContext) {
+								JsonObject err = (JsonObject)ec;
+								addSoftErrorByFile(profile,
+										err.getString("reason"), "" + (i + 1), err.getString("attribute"), err.getString("value"));
+							}
 						}
+						final String classesStr = Joiner.on(", ").join(classesNames);
+						classesNamesMapping.put(user.getString("externalId"), classesStr);
+						addUser(profile, user.put("state", translate(state.name()))
+										.put("translatedProfile", translate(profile))
+										.put("classesStr", classesStr)
+										.put("childExternalId", linkStudents)
+										.put("line", i + 1)
+						);
 						i++;
 					}
 				} catch (Exception e) {