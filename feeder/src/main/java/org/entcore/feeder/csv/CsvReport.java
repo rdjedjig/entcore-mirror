simbas/entcore

[Evo][Feeder] update errors persistance

feeder/src/main/java/org/entcore/feeder/csv/CsvReport.java
@@ -44,7 +44,6 @@ public class CsvReport extends Report {
 	private static final String MAPPINGS = "mappings";
 	private static final String CLASSES_MAPPING = "classesMapping";
 	private static final String HEADERS = "headers";
-	public static final String KEYS_CLEANED = "keysCleaned";
 	private final Vertx vertx;
 	protected final ProfileColumnsMapper columnsMapper;
 
@@ -216,11 +215,22 @@ public class CsvReport extends Report {
 		}
 	}
 
+	@Override
+	protected boolean updateCleanKeys() {
+		return (cleanAttributeKeys(result.getObject("errors")) + cleanAttributeKeys(result.getObject("softErrors"))) > 0;
+	}
+
 	protected void uncleanKeys() {
 		uncleanAttributeKeys(getClassesMappings());
 		uncleanAttributeKeys(getMappings());
 		uncleanAttributeKeys(result.getObject("errors"));
 		result.removeField(KEYS_CLEANED);
+	}
+
+	protected void clearBeforeValidation() {
+		result.putObject("errors", new JsonObject())
+				.putObject(FILES, new JsonObject())
+				.putObject("softErrors", new JsonObject());
 	}
 
 //	protected void setStructureExternalIdIfAbsent(String structureExternalId) {

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -36,6 +36,7 @@ public class Report {
 	public static final Logger log = LoggerFactory.getLogger(Report.class);
 	public static final String FILES = "files";
 	public static final String PROFILES = "profiles";
+	public static final String KEYS_CLEANED = "keysCleaned";
 	public final JsonObject result;
 	private final I18n i18n = I18n.getInstance();
 	public final String acceptLanguage;
@@ -170,7 +171,21 @@ public class Report {
 		MongoDb.getInstance().save("imports", this.getResult(), handler);
 	}
 
+	public void updateErrors(Handler<Message<JsonObject>> handler) {
+		boolean cleaned = updateCleanKeys();
+		JsonObject modif = new JsonObject()
+				.putObject("errors", result.getObject("errors"))
+				.putObject("softErrors", result.getObject("softErrors"));
+		if (cleaned) {
+			modif.putBoolean(KEYS_CLEANED, true);
+		}
+		MongoDb.getInstance().update("imports", new JsonObject().putString("_id", result.getString("_id")),
+				new JsonObject().putObject("$set", modif), handler);
+	}
+
 	protected void cleanKeys() {}
+
+	protected boolean updateCleanKeys() { return false; }
 
 	protected int cleanAttributeKeys(JsonObject attribute) {
 		int count = 0;

rdjedjig/entcore-mirror

feeder/src/main/java/org/entcore/feeder/csv/CsvReport.java
@@ -44,7 +44,6 @@ public class CsvReport extends Report {
 	private static final String MAPPINGS = "mappings";
 	private static final String CLASSES_MAPPING = "classesMapping";
 	private static final String HEADERS = "headers";
-	public static final String KEYS_CLEANED = "keysCleaned";
 	private final Vertx vertx;
 	protected final ProfileColumnsMapper columnsMapper;
 
@@ -216,11 +215,22 @@ public class CsvReport extends Report {
 		}
 	}
 
+	@Override
+	protected boolean updateCleanKeys() {
+		return (cleanAttributeKeys(result.getJsonObject("errors")) + cleanAttributeKeys(result.getJsonObject("softErrors"))) > 0;
+	}
+
 	protected void uncleanKeys() {
 		uncleanAttributeKeys(getClassesMappings());
 		uncleanAttributeKeys(getMappings());
 		uncleanAttributeKeys(result.getJsonObject("errors"));
 		result.remove(KEYS_CLEANED);
+	}
+
+	protected void clearBeforeValidation() {
+		result.put("errors", new JsonObject())
+				.put(FILES, new JsonObject())
+				.put("softErrors", new JsonObject());
 	}
 
 //	protected void setStructureExternalIdIfAbsent(String structureExternalId) {

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -56,6 +56,7 @@ public class Report {
 	public static final String FILES = "files";
 	public static final String PROFILES = "profiles";
 	private static final String MAPPINGS = "mappings";
+	public static final String KEYS_CLEANED = "keysCleaned";
 	public final JsonObject result;
 	private final I18n i18n = I18n.getInstance();
 	public final String acceptLanguage;
@@ -197,8 +198,19 @@ public class Report {
 		MongoDb.getInstance().save("imports", this.getResult(), handler);
 	}
 
-	protected void cleanKeys() {
+	public void updateErrors(Handler<Message<JsonObject>> handler) {
+		boolean cleaned = updateCleanKeys();
+		JsonObject modif = new JsonObject()
+				.put("errors", result.getJsonObject("errors"))
+				.put("softErrors", result.getJsonObject("softErrors"));
+		if (cleaned) {
+			modif.put(KEYS_CLEANED, true);
+		}
+		MongoDb.getInstance().update("imports", new JsonObject().put("_id", result.getString("_id")),
+				new JsonObject().put("$set", modif), handler);
 	}
+
+	protected void cleanKeys() {}
 
 	public void setEndTime(long endTime) {
 		this.endTime = endTime;
@@ -389,6 +401,8 @@ public class Report {
 			result.put(MAPPINGS, mappings);
 		}
 	}
+
+	protected boolean updateCleanKeys() { return false; }
 
 	protected int cleanAttributeKeys(JsonObject attribute) {
 		int count = 0;