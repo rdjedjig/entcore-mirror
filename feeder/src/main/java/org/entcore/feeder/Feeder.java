simbas/entcore

[Fix][Feeder] Fix Column Mapping result output

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -403,13 +403,13 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		v.columnsMapping(path, new Handler<JsonObject>() {
 			@Override
 			public void handle(JsonObject event) {
+				JsonObject result = new JsonObject().putObject("result",
+						v.getResult().putObject("availableFields", v.getColumnsMapper().availableFields())
+				);
 				if (!v.containsErrors()) {
-					sendOK(message, new JsonObject()
-							.putObject("mappings", v.getMappings())
-							.putObject("availableFields", v.getColumnsMapper().availableFields()));
-				} else {
-					sendOK(message, new JsonObject().putObject("result", v.getResult()));
+					result.getObject("result").removeField("errors");
 				}
+				sendOK(message, result);
 			}
 		});
 	}

rdjedjig/entcore-mirror

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -413,14 +413,13 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		v.columnsMapping(path, new Handler<JsonObject>() {
 			@Override
 			public void handle(JsonObject event) {
+				JsonObject result = new JsonObject().put("result",
+						v.getResult().put("availableFields", v.getColumnsMapper().availableFields())
+				);
 				if (!v.containsErrors()) {
-					sendOK(message, new JsonObject()
-							.put("mappings", v.getMappings())
-							.put("availableFields", v.getColumnsMapper().availableFields()));
-				} else {
-//					sendError(message, "column.mapping.error");
-					sendOK(message, new JsonObject().put("result", v.getResult()));
+					result.getJsonObject("result").remove("errors");
 				}
+				sendOK(message, result);
 			}
 		});
 	}