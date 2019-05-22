simbas/entcore

[Fix][Feeder] Fix persist when errors occurs.

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -354,9 +354,13 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 					event.result().exportIfValid(new Handler<JsonObject>() {
 						@Override
 						public void handle(JsonObject event) {
-							// TODO check error
-							message.body().mergeIn(event);
-							launchImport(message);
+							final JsonObject errors = event.getObject("errors");
+							if (errors != null && errors.size() > 0) {
+								sendOK(message, new JsonObject().putObject("result", event));
+							} else {
+								message.body().mergeIn(event);
+								launchImport(message);
+							}
 						}
 					});
 				} else {

feeder/src/main/java/org/entcore/feeder/csv/CsvReport.java
@@ -210,6 +210,7 @@ public class CsvReport extends Report {
 		int count = 0;
 		count += cleanAttributeKeys(getClassesMappings());
 		count += cleanAttributeKeys(getMappings());
+		count += cleanAttributeKeys(result.getObject("errors"));
 		if (count > 0) {
 			result.putBoolean(KEYS_CLEANED, true);
 		}
@@ -218,6 +219,7 @@ public class CsvReport extends Report {
 	protected void uncleanKeys() {
 		uncleanAttributeKeys(getClassesMappings());
 		uncleanAttributeKeys(getMappings());
+		uncleanAttributeKeys(result.getObject("errors"));
 		result.removeField(KEYS_CLEANED);
 	}
 

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -175,15 +175,20 @@ public class Report {
 	protected int cleanAttributeKeys(JsonObject attribute) {
 		int count = 0;
 		if (attribute != null) {
-			for (String attr : attribute.getFieldNames()) {
-				JsonObject j = attribute.getObject(attr);
-				if (j != null) {
-					for (String attr2 : j.copy().getFieldNames()) {
+			for (String attr : attribute.copy().getFieldNames()) {
+				Object j = attribute.getValue(attr);
+				if (j instanceof JsonObject) {
+					for (String attr2 : ((JsonObject) j).copy().getFieldNames()) {
 						if (attr2.contains(".")) {
 							count++;
-							j.putString(attr2.replaceAll("\\.", "_|_"), (String) j.removeField(attr2));
+							((JsonObject) j).putString(
+									attr2.replaceAll("\\.", "_|_"), (String) ((JsonObject) j).removeField(attr2));
 						}
 					}
+				} else if (j instanceof JsonArray && attr.contains(".")) {
+					attribute.putArray(attr.replaceAll("\\.", "_|_"), (JsonArray) j);
+					attribute.removeField(attr);
+					count++;
 				}
 			}
 		}
@@ -192,14 +197,18 @@ public class Report {
 
 	protected void uncleanAttributeKeys(JsonObject attribute) {
 		if (attribute != null) {
-			for (String attr : attribute.getFieldNames()) {
-				JsonObject j = attribute.getObject(attr);
-				if (j != null) {
-					for (String attr2 : j.copy().getFieldNames()) {
+			for (String attr : attribute.copy().getFieldNames()) {
+				Object j = attribute.getValue(attr);
+				if (j instanceof JsonObject) {
+					for (String attr2 : ((JsonObject) j).copy().getFieldNames()) {
 						if (attr2.contains("_|_")) {
-							j.putString(attr2.replaceAll("_\\|_", "."), (String) j.removeField(attr2));
+							((JsonObject) j).putString(
+									attr2.replaceAll("_\\|_", "."), (String) ((JsonObject) j).removeField(attr2));
 						}
 					}
+				} else if (j instanceof JsonArray && attr.contains("_|_")) {
+					attribute.putArray(attr.replaceAll("_\\|_", "."), (JsonArray) j);
+					attribute.removeField(attr);
 				}
 			}
 		}

rdjedjig/entcore-mirror

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -366,9 +366,13 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 					event.result().exportIfValid(new Handler<JsonObject>() {
 						@Override
 						public void handle(JsonObject event) {
-							// TODO check error
-							message.body().mergeIn(event);
-							launchImport(message);
+							final JsonObject errors = event.getJsonObject("errors");
+							if (errors != null && errors.size() > 0) {
+								sendOK(message, new JsonObject().put("result", event));
+							} else {
+								message.body().mergeIn(event);
+								launchImport(message);
+							}
 						}
 					});
 				} else {

feeder/src/main/java/org/entcore/feeder/csv/CsvReport.java
@@ -210,6 +210,7 @@ public class CsvReport extends Report {
 		int count = 0;
 		count += cleanAttributeKeys(getClassesMappings());
 		count += cleanAttributeKeys(getMappings());
+		count += cleanAttributeKeys(result.getJsonObject("errors"));
 		if (count > 0) {
 			result.put(KEYS_CLEANED, true);
 		}
@@ -218,6 +219,7 @@ public class CsvReport extends Report {
 	protected void uncleanKeys() {
 		uncleanAttributeKeys(getClassesMappings());
 		uncleanAttributeKeys(getMappings());
+		uncleanAttributeKeys(result.getJsonObject("errors"));
 		result.remove(KEYS_CLEANED);
 	}
 

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -26,6 +26,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import org.apache.commons.lang3.StringUtils;
 import org.entcore.common.email.EmailFactory;
 import org.entcore.common.http.request.JsonHttpServerRequest;
@@ -393,13 +394,20 @@ public class Report {
 		int count = 0;
 		if (attribute != null) {
 			for (String attr : attribute.fieldNames()) {
-				JsonObject j = attribute.getJsonObject(attr);
-				if (j != null) {
-					for (String attr2 : j.copy().fieldNames()) {
-						if (attr2.contains(".")) {
-							count++;
-							j.put(attr2.replaceAll("\\.", "_|_"), (String) j.remove(attr2));
+				Object j = attribute.getValue(attr);
+				if (j != null){
+					if (j instanceof JsonObject) {
+						JsonObject jo = (JsonObject)j;
+						for (String attr2 : jo.copy().fieldNames()) {
+							if (attr2.contains(".")) {
+								count++;
+								jo.put(attr2.replaceAll("\\.", "_|_"), (String) jo.remove(attr2));
+							}
 						}
+					} else if (j instanceof JsonArray && attr.contains(".")) {
+						attribute.put(attr.replaceAll("\\.", "_|_"), (JsonArray) j);
+						attribute.remove(attr);
+						count++;
 					}
 				}
 			}
@@ -410,12 +418,18 @@ public class Report {
 	protected void uncleanAttributeKeys(JsonObject attribute) {
 		if (attribute != null) {
 			for (String attr : attribute.fieldNames()) {
-				JsonObject j = attribute.getJsonObject(attr);
+				Object j = attribute.getValue(attr);
 				if (j != null) {
-					for (String attr2 : j.copy().fieldNames()) {
-						if (attr2.contains("_|_")) {
-							j.put(attr2.replaceAll("_\\|_", "."), (String) j.remove(attr2));
+					if (j instanceof JsonObject) {
+						JsonObject jo = (JsonObject)j;
+						for (String attr2 : jo.copy().fieldNames()) {
+							if (attr2.contains("_|_")) {
+								jo.put(attr2.replaceAll("_\\|_", "."), (String) jo.remove(attr2));
+							}
 						}
+					} else if (j instanceof JsonArray && attr.contains("_|_")) {
+						attribute.put(attr.replaceAll("_\\|_", "."), (JsonArray) j);
+						attribute.remove(attr);
 					}
 				}
 			}