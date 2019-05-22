simbas/entcore

[Feeder] [fix] Change soft errors report to ease context error highlight

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -111,27 +111,34 @@ public class Report {
 			softErrors = new JsonObject();
 			result.putObject("softErrors", softErrors);
 		}
-		JsonObject fileErrors = softErrors.getObject(file);
+		JsonArray reasons = softErrors.getArray("reasons");
+		if (reasons == null) {
+			reasons = new JsonArray();
+			softErrors.putArray("reasons", reasons);
+		}
+		if (!reasons.contains(key)) {
+			reasons.addString(key);
+		}
+
+		JsonArray fileErrors = softErrors.getArray(file);
 		if (fileErrors == null) {
-			fileErrors = new JsonObject();
-			softErrors.putObject(file, fileErrors);
+			fileErrors = new JsonArray();
+			softErrors.putArray(file, fileErrors);
 		}
-		String cleanKey = key.replace('.','-'); // Mongo don't support '.' characters in document field's name
-		JsonObject reason = fileErrors.getObject(cleanKey);
-		if (reason == null) {
-			reason = new JsonObject();
-			fileErrors.putObject(cleanKey, reason);
-		}
-		JsonArray lineErrors = reason.getArray(lineNumber);
-		if (lineErrors == null) {
-			lineErrors = new JsonArray();
-			reason.putArray(lineNumber, lineErrors);
-		}
+		JsonObject error = new JsonObject().copy()
+				.putString("line",lineNumber)
+				.putString("reason", key)
+				.putString("attribute", errors.length > 0 ? errors[0] : "")
+				.putString("value", errors.length > 1 ? errors[1] : "");
+
 		List<String> errorContext = new ArrayList<>(Arrays.asList(errors)); // Hack to support "add" operation
 		errorContext.add(0, lineNumber);
-		String error = i18n.translate(key, I18n.DEFAULT_DOMAIN, acceptLanguage, errorContext.toArray(new String[errorContext.size()]));
-		lineErrors.addString(error);
-		log.error(error);
+		String translation = i18n.translate(key, I18n.DEFAULT_DOMAIN, acceptLanguage, errorContext.toArray(new String[errorContext.size()]));
+		error.putString("translation", translation);
+
+		fileErrors.addObject(error);
+		log.error(translation);
+		//String cleanKey = key.replace('.','-'); // Mongo don't support '.' characters in document field's name
 	}
 
 	public void addUser(String file, JsonObject props) {

feeder/src/main/java/org/entcore/feeder/utils/Validator.java
@@ -239,7 +239,7 @@ public class Validator {
 				if (errorsContext != null) {
 					errorsContext.addObject(new JsonObject()
 							.putString("reason", "missing.attribute")
-							.putString("attribute", i18n.translate(o.toString(), I18n.DEFAULT_DOMAIN, acceptLanguage))
+							.putString("attribute", o.toString())
 					);
 				}
 				return i18n.translate("missing.attribute", I18n.DEFAULT_DOMAIN, acceptLanguage,
@@ -424,7 +424,7 @@ public class Validator {
 			if (errorsContext != null) {
 				errorsContext.addObject(new JsonObject()
 						.putString("reason", "invalid.value")
-						.putString("attribute", i18n.translate(attr, I18n.DEFAULT_DOMAIN, acceptLanguage))
+						.putString("attribute", attr)
 						.putString("value", (value != null ? value.toString() : "null"))
 				);
 			}

rdjedjig/entcore-mirror

feeder/src/main/java/org/entcore/feeder/utils/Report.java
@@ -122,27 +122,34 @@ public class Report {
 			softErrors = new JsonObject();
 			result.put("softErrors", softErrors);
 		}
-		JsonObject fileErrors = softErrors.getJsonObject(file);
+		JsonArray reasons = softErrors.getJsonArray("reasons");
+		if (reasons == null) {
+			reasons = new JsonArray();
+			softErrors.put("reasons", reasons);
+		}
+		if (!reasons.contains(key)) {
+			reasons.add(key);
+		}
+
+		JsonArray fileErrors = softErrors.getJsonArray(file);
 		if (fileErrors == null) {
-			fileErrors = new JsonObject();
+			fileErrors = new JsonArray();
 			softErrors.put(file, fileErrors);
 		}
-		String cleanKey = key.replace('.','-'); // Mongo don't support '.' characters in document field's name
-		JsonObject reason = fileErrors.getJsonObject(cleanKey);
-		if (reason == null) {
-			reason = new JsonObject();
-			fileErrors.put(cleanKey, reason);
-		}
-		JsonArray lineErrors = reason.getJsonArray(lineNumber);
-		if (lineErrors == null) {
-			lineErrors = new JsonArray();
-			reason.put(lineNumber, lineErrors);
-		}
+		JsonObject error = new JsonObject().copy()
+				.put("line",lineNumber)
+				.put("reason", key)
+				.put("attribute", errors.length > 0 ? errors[0] : "")
+				.put("value", errors.length > 1 ? errors[1] : "");
+
 		List<String> errorContext = new ArrayList<>(Arrays.asList(errors)); // Hack to support "add" operation
 		errorContext.add(0, lineNumber);
-		String error = i18n.translate(key, I18n.DEFAULT_DOMAIN, acceptLanguage, errorContext.toArray(new String[errorContext.size()]));
-		lineErrors.add(error);
-		log.error(error);
+		String translation = i18n.translate(key, I18n.DEFAULT_DOMAIN, acceptLanguage, errorContext.toArray(new String[errorContext.size()]));
+		error.put("translation", translation);
+
+		fileErrors.add(error);
+		log.error(translation);
+		//String cleanKey = key.replace('.','-'); // Mongo don't support '.' characters in document field's name
 	}
 
 	public void addUser(String file, JsonObject props) {
@@ -221,6 +228,7 @@ public class Report {
 		}
 		MongoDb.getInstance().update("imports", new JsonObject().put("_id", result.getString("_id")),
 				new JsonObject().put("$set", modif), handler);
+
 	}
 
 	protected void cleanKeys() {}

feeder/src/main/java/org/entcore/feeder/utils/Validator.java
@@ -489,7 +489,7 @@ public class Validator {
 				if (errorsContext != null) {
 					errorsContext.add(new JsonObject()
 							.put("reason", "invalid.value")
-							.put("attribute", i18n.translate(attr, I18n.DEFAULT_DOMAIN, acceptLanguage))
+							.put("attribute", attr)
 							.put("value", (value != null ? value.toString() : "null"))
 					);
 				}