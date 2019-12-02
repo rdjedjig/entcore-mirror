sjollois/entcore

build(adminv2): #CAV2-478, upgrade to Angular 8.2

admin/src/main/ts/src/app/app-routing.module.ts
@@ -1,0 +1,30 @@
+import {AppHomeComponent} from './app-home.component';
+import {NavComponent} from './core/nav/nav.component';
+import {ConfigResolver} from './core/resolvers/config.resolver';
+import {I18nResolver} from './core/resolvers/i18n.resolver';
+import {StructuresResolver} from './core/resolvers/structures.resolver';
+import {SessionResolver} from './core/resolvers/session.resolver';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  {
+      path: 'app',
+      resolve: {session: SessionResolver, structures: StructuresResolver, i18n: I18nResolver, config: ConfigResolver},
+      component: NavComponent,
+      children: [
+          {path: '', component: AppHomeComponent},
+          {path: ':structureId', loadChildren: () => import('./structure/structure.module').then(m => m.StructureModule)}
+      ]
+  },
+  {
+      path: '**',
+      redirectTo: '/app'
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

admin/src/main/ts/src/app/core/nav/nav.component.ts
@@ -1,0 +1,71 @@
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
+
+import {globalStore, StructureModel} from '../store';
+import {Subscription} from 'rxjs';
+import {Config} from '../resolvers/Config';
+
+@Component({
+    selector: 'ode-app-nav',
+    templateUrl: './nav.component.html'
+})
+export class NavComponent implements OnInit, OnDestroy {
+    // tslint:disable-next-line:variable-name
+    private _currentStructure: StructureModel;
+    set currentStructure(struct: StructureModel) {
+        this._currentStructure = struct;
+
+        const replacerRegex = /^\/{0,1}app(\/[^\/]+){0,1}/;
+        const newPath = window.location.pathname.replace(replacerRegex, `/app/${struct.id}`);
+
+        this.router.navigateByUrl(newPath);
+    }
+    get currentStructure() { return this._currentStructure; }
+    openside: boolean;
+    structureFilter: string;
+    structures: StructureModel[];
+
+    @ViewChild('sidePanelOpener', { static: false }) sidePanelOpener: ElementRef;
+
+    private structureSubscriber: Subscription;
+    private routeSubscription: Subscription;
+
+    public config: Config;
+
+    constructor(
+        public router: Router,
+        private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.structures = globalStore.structures.asTree();
+
+        if (this.structures.length === 1 && !this.structures[0].children) {
+            this.currentStructure = this.structures[0];
+        }
+
+        this.structureSubscriber = this.route.children[0].params.subscribe(params => {
+            const structureId = params.structureId;
+            if (structureId) {
+                this.currentStructure = globalStore.structures.data.find(
+                    s => s.id === structureId);
+            }
+        });
+
+        this.routeSubscription = this.route.data.subscribe((data: Data) => {
+            this.config = data.config;
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.structureSubscriber) {
+            this.structureSubscriber.unsubscribe();
+        }
+        if (this.routeSubscription) {
+            this.routeSubscription.unsubscribe();
+        }
+    }
+
+    public openReports(): void {
+        window.open('/timeline/admin-history', '_blank');
+    }
+}

admin/src/main/ts/src/app/services/services.component.ts
@@ -1,0 +1,49 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {ServicesStore} from './services.store';
+import {routing} from '../core/services';
+import {ServicesService} from './services.service';
+
+@Component({
+    selector: 'ode-services-root',
+    templateUrl: './services.component.html',
+    providers: [ServicesService]
+})
+export class ServicesComponent implements OnInit, OnDestroy {
+
+    private structureSubscriber: Subscription;
+
+    tabs: Array<{ label: string, view: string, disabled: boolean }> = [
+        {label: 'applications', view: 'applications', disabled: false},
+        {label: 'connectors', view: 'connectors', disabled: false}
+    ];
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private servicesStore: ServicesStore) {
+    }
+
+    ngOnInit(): void {
+        this.structureSubscriber = routing.observe(this.route, 'data').subscribe((data: Data) => {
+            if (data.structure) {
+                this.servicesStore.structure = data.structure;
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.structureSubscriber.unsubscribe();
+    }
+
+    public showCreateConnectorButton(): boolean {
+        if (this.router.isActive(`/app/${this.servicesStore.structure.id}/services/connectors/create`, true)) {
+            return false;
+        }
+        if (this.router.isActive(`/app/${this.servicesStore.structure.id}/services/connectors`, false)) {
+            return true;
+        }
+        return false;
+    }
+}

admin/src/main/ts/src/app/users/create/user-create.component.ts
@@ -1,0 +1,120 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {Subscription} from 'rxjs';
+
+import {UsersStore} from '../users.store';
+import {routing} from '../../core/services/routing.service';
+import {UserModel} from '../../core/store/models/user.model';
+import {NotifyService, SpinnerService, UserChildrenListService, UserListService} from '../../core/services';
+import {SelectOption} from '../../shared/ux/components/multi-select/multi-select.component';
+
+@Component({
+    selector: 'ode-user-create',
+    templateUrl: './user-create.component.html'
+    ,
+    providers: [UserChildrenListService]
+})
+export class UserCreateComponent implements OnInit, OnDestroy {
+
+    newUser: UserModel = new UserModel();
+    noClasses: Array<any> = [];
+    private structureSubscriber: Subscription;
+
+    public typeOptions: SelectOption<string>[] = ['Teacher', 'Personnel', 'Relative', 'Student', 'Guest'].map(t => ({
+        value: t,
+        label: t
+    }));
+    public classeOptions: SelectOption<{ id: string, name: string }[]>[] = [];
+
+    constructor(
+        public usersStore: UsersStore,
+        private ns: NotifyService,
+        private spinner: SpinnerService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private location: Location,
+        private userListService: UserListService,
+        public userChildrenListService: UserChildrenListService) {
+    }
+
+    ngOnInit(): void {
+        this.usersStore.user = null;
+        this.newUser.classes = null;
+        this.newUser.type = 'Personnel';
+        const {id, name} = this.usersStore.structure;
+        this.newUser.structures = [{id, name}];
+        this.classeOptions = [{value: null, label: 'create.user.sansclasse'}];
+
+        this.structureSubscriber = routing.observe(this.route, 'data').subscribe((data: Data) => {
+            if (data.structure) {
+
+                this.newUser.structures = [data.structure];
+                this.classeOptions = [{value: null, label: 'create.user.sansclasse'}];
+                this.classeOptions.push(...this.usersStore.structure.classes.map(c => ({value: [c], label: c.name})));
+            }
+        });
+        this.newUser.userDetails.children = [];
+    }
+
+    ngOnDestroy(): void {
+        this.structureSubscriber.unsubscribe();
+    }
+
+    createNewUser() {
+        this.spinner.perform('portal-content', this.newUser.createNewUser(this.usersStore.structure.id)
+            .then(res => {
+                this.ns.success({
+                        key: 'notify.user.create.content',
+                        parameters: {
+                            user: this.newUser.firstName + ' ' + this.newUser.lastName
+                        }
+                    }
+                    , 'notify.user.create.title');
+
+                this.newUser.id = res.data.id;
+                this.newUser.source = 'MANUAL';
+                this.newUser.displayName = `${this.newUser.lastName} ${this.newUser.firstName}`;
+                if (this.newUser.classes == null) {
+                    this.newUser.classes = [];
+                }
+                this.usersStore.structure.users.data.push(this.newUser);
+
+                this.router.navigate(['/app', this.usersStore.structure.id, 'users', res.data.id, 'details'], {
+                    relativeTo: this.route,
+                    replaceUrl: false
+                });
+            }).catch(err => {
+                this.ns.error({
+                        key: 'notify.user.create.error.content',
+                        parameters: {
+                            user: this.newUser.firstName + ' ' + this.newUser.lastName
+                        }
+                    }
+                    , 'notify.user.create.error.title', err);
+            })
+        );
+    }
+
+    addChild(child) {
+        if (this.newUser.userDetails.children.indexOf(child) < 0) {
+            this.newUser.userDetails.children.push(child);
+        }
+    }
+
+    removeChild(child) {
+        const index = this.newUser.userDetails.children.indexOf(child);
+        this.newUser.userDetails.children.splice(index, 1);
+    }
+
+    cancel() {
+        this.location.back();
+    }
+
+    trim(input: string) {
+        if (input && input.length > 0) {
+            return input.trim();
+        }
+        return input;
+    }
+}

admin/src/main/ts/src/app/users/details/user-details.resolver.ts
@@ -1,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, Router} from '@angular/router';
+import {routing, SpinnerService} from '../../core/services';
+import {globalStore, UserModel} from '../../core/store';
+
+@Injectable()
+export class UserDetailsResolver implements Resolve<UserModel | Error> {
+
+    constructor(private spinner: SpinnerService, private router: Router) {
+    }
+
+    resolve(route: ActivatedRouteSnapshot): Promise<UserModel> {
+        const structure = globalStore.structures.data.find(s => s.id === routing.getParam(route, 'structureId'));
+        const user = structure &&
+            structure.users.data.find(u => u.id === route.params.userId);
+
+        if (user) {
+            return this.spinner.perform('portal-content', user.userDetails.sync()
+                .catch((err) => {
+                    this.router.navigate(['/app', structure.id, 'users'], {replaceUrl: false});
+                }).then(() => {
+                    return user;
+                }));
+        } else {
+            this.router.navigate(['/app', structure.id, 'users', 'filter'], {replaceUrl: false});
+        }
+    }
+}

rdjedjig/entcore-mirror

admin/src/main/ts/src/app/app-routing.module.ts
@@ -1,0 +1,30 @@
+import {AppHomeComponent} from './app-home.component';
+import {NavComponent} from './core/nav/nav.component';
+import {ConfigResolver} from './core/resolvers/config.resolver';
+import {I18nResolver} from './core/resolvers/i18n.resolver';
+import {StructuresResolver} from './core/resolvers/structures.resolver';
+import {SessionResolver} from './core/resolvers/session.resolver';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  {
+      path: 'admin',
+      resolve: {session: SessionResolver, structures: StructuresResolver, i18n: I18nResolver, config: ConfigResolver},
+      component: NavComponent,
+      children: [
+          {path: '', component: AppHomeComponent},
+          {path: ':structureId', loadChildren: () => import('./structure/structure.module').then(m => m.StructureModule)}
+      ]
+  },
+  {
+      path: '**',
+      redirectTo: '/admin'
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

admin/src/main/ts/src/app/core/nav/nav.component.ts
@@ -1,0 +1,72 @@
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
+
+import {globalStore, StructureModel} from '../store';
+import {Subscription} from 'rxjs';
+import {Config} from '../resolvers/Config';
+
+@Component({
+    selector: 'ode-app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit, OnDestroy {
+    // tslint:disable-next-line:variable-name
+    private _currentStructure: StructureModel;
+    set currentStructure(struct: StructureModel) {
+        this._currentStructure = struct;
+
+        const replacerRegex = /^\/{0,1}admin(\/[^\/]+){0,1}/;
+        const newPath = window.location.pathname.replace(replacerRegex, `/admin/${struct.id}`);
+
+        this.router.navigateByUrl(newPath);
+    }
+    get currentStructure() { return this._currentStructure; }
+    openside: boolean;
+    structureFilter: string;
+    structures: StructureModel[];
+
+    @ViewChild('sidePanelOpener', { static: false }) sidePanelOpener: ElementRef;
+
+    private structureSubscriber: Subscription;
+    private routeSubscription: Subscription;
+
+    public config: Config;
+
+    constructor(
+        public router: Router,
+        private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.structures = globalStore.structures.asTree();
+
+        if (this.structures.length === 1 && !this.structures[0].children) {
+            this.currentStructure = this.structures[0];
+        }
+
+        this.structureSubscriber = this.route.children[0].params.subscribe(params => {
+            const structureId = params.structureId;
+            if (structureId) {
+                this.currentStructure = globalStore.structures.data.find(
+                    s => s.id === structureId);
+            }
+        });
+
+        this.routeSubscription = this.route.data.subscribe((data: Data) => {
+            this.config = data.config;
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.structureSubscriber) {
+            this.structureSubscriber.unsubscribe();
+        }
+        if (this.routeSubscription) {
+            this.routeSubscription.unsubscribe();
+        }
+    }
+
+    public openReports(): void {
+        window.open('/timeline/admin-history', '_blank');
+    }
+}

admin/src/main/ts/src/app/services/services.component.ts
@@ -1,0 +1,49 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {ServicesStore} from './services.store';
+import {routing} from '../core/services';
+import {ServicesService} from './services.service';
+
+@Component({
+    selector: 'ode-services-root',
+    templateUrl: './services.component.html',
+    providers: [ServicesService]
+})
+export class ServicesComponent implements OnInit, OnDestroy {
+
+    private structureSubscriber: Subscription;
+
+    tabs: Array<{ label: string, view: string, disabled: boolean }> = [
+        {label: 'applications', view: 'applications', disabled: false},
+        {label: 'connectors', view: 'connectors', disabled: false}
+    ];
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private servicesStore: ServicesStore) {
+    }
+
+    ngOnInit(): void {
+        this.structureSubscriber = routing.observe(this.route, 'data').subscribe((data: Data) => {
+            if (data.structure) {
+                this.servicesStore.structure = data.structure;
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.structureSubscriber.unsubscribe();
+    }
+
+    public showCreateConnectorButton(): boolean {
+        if (this.router.isActive(`/admin/${this.servicesStore.structure.id}/services/connectors/create`, true)) {
+            return false;
+        }
+        if (this.router.isActive(`/admin/${this.servicesStore.structure.id}/services/connectors`, false)) {
+            return true;
+        }
+        return false;
+    }
+}

admin/src/main/ts/src/app/users/create/user-create.component.ts
@@ -1,0 +1,120 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {Subscription} from 'rxjs';
+
+import {UsersStore} from '../users.store';
+import {routing} from '../../core/services/routing.service';
+import {UserModel} from '../../core/store/models/user.model';
+import {NotifyService, SpinnerService, UserChildrenListService, UserListService} from '../../core/services';
+import {SelectOption} from '../../shared/ux/components/multi-select/multi-select.component';
+
+@Component({
+    selector: 'ode-user-create',
+    templateUrl: './user-create.component.html'
+    ,
+    providers: [UserChildrenListService]
+})
+export class UserCreateComponent implements OnInit, OnDestroy {
+
+    newUser: UserModel = new UserModel();
+    noClasses: Array<any> = [];
+    private structureSubscriber: Subscription;
+
+    public typeOptions: SelectOption<string>[] = ['Teacher', 'Personnel', 'Relative', 'Student', 'Guest'].map(t => ({
+        value: t,
+        label: t
+    }));
+    public classeOptions: SelectOption<{ id: string, name: string }[]>[] = [];
+
+    constructor(
+        public usersStore: UsersStore,
+        private ns: NotifyService,
+        private spinner: SpinnerService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private location: Location,
+        private userListService: UserListService,
+        public userChildrenListService: UserChildrenListService) {
+    }
+
+    ngOnInit(): void {
+        this.usersStore.user = null;
+        this.newUser.classes = null;
+        this.newUser.type = 'Personnel';
+        const {id, name} = this.usersStore.structure;
+        this.newUser.structures = [{id, name}];
+        this.classeOptions = [{value: null, label: 'create.user.sansclasse'}];
+
+        this.structureSubscriber = routing.observe(this.route, 'data').subscribe((data: Data) => {
+            if (data.structure) {
+
+                this.newUser.structures = [data.structure];
+                this.classeOptions = [{value: null, label: 'create.user.sansclasse'}];
+                this.classeOptions.push(...this.usersStore.structure.classes.map(c => ({value: [c], label: c.name})));
+            }
+        });
+        this.newUser.userDetails.children = [];
+    }
+
+    ngOnDestroy(): void {
+        this.structureSubscriber.unsubscribe();
+    }
+
+    createNewUser() {
+        this.spinner.perform('portal-content', this.newUser.createNewUser(this.usersStore.structure.id)
+            .then(res => {
+                this.ns.success({
+                        key: 'notify.user.create.content',
+                        parameters: {
+                            user: this.newUser.firstName + ' ' + this.newUser.lastName
+                        }
+                    }
+                    , 'notify.user.create.title');
+
+                this.newUser.id = res.data.id;
+                this.newUser.source = 'MANUAL';
+                this.newUser.displayName = `${this.newUser.lastName} ${this.newUser.firstName}`;
+                if (this.newUser.classes == null) {
+                    this.newUser.classes = [];
+                }
+                this.usersStore.structure.users.data.push(this.newUser);
+
+                this.router.navigate(['/admin', this.usersStore.structure.id, 'users', res.data.id, 'details'], {
+                    relativeTo: this.route,
+                    replaceUrl: false
+                });
+            }).catch(err => {
+                this.ns.error({
+                        key: 'notify.user.create.error.content',
+                        parameters: {
+                            user: this.newUser.firstName + ' ' + this.newUser.lastName
+                        }
+                    }
+                    , 'notify.user.create.error.title', err);
+            })
+        );
+    }
+
+    addChild(child) {
+        if (this.newUser.userDetails.children.indexOf(child) < 0) {
+            this.newUser.userDetails.children.push(child);
+        }
+    }
+
+    removeChild(child) {
+        const index = this.newUser.userDetails.children.indexOf(child);
+        this.newUser.userDetails.children.splice(index, 1);
+    }
+
+    cancel() {
+        this.location.back();
+    }
+
+    trim(input: string) {
+        if (input && input.length > 0) {
+            return input.trim();
+        }
+        return input;
+    }
+}

admin/src/main/ts/src/app/users/details/user-details.resolver.ts
@@ -1,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, Router} from '@angular/router';
+import {routing, SpinnerService} from '../../core/services';
+import {globalStore, UserModel} from '../../core/store';
+
+@Injectable()
+export class UserDetailsResolver implements Resolve<UserModel | Error> {
+
+    constructor(private spinner: SpinnerService, private router: Router) {
+    }
+
+    resolve(route: ActivatedRouteSnapshot): Promise<UserModel> {
+        const structure = globalStore.structures.data.find(s => s.id === routing.getParam(route, 'structureId'));
+        const user = structure &&
+            structure.users.data.find(u => u.id === route.params.userId);
+
+        if (user) {
+            return this.spinner.perform('portal-content', user.userDetails.sync()
+                .catch((err) => {
+                    this.router.navigate(['/admin', structure.id, 'users'], {replaceUrl: false});
+                }).then(() => {
+                    return user;
+                }));
+        } else {
+            this.router.navigate(['/admin', structure.id, 'users', 'filter'], {replaceUrl: false});
+        }
+    }
+}