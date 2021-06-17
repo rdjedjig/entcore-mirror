4081febc1ec9ff737f7eaf143c50615015c4dc24
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-06-17 08:02:09 +0200

    fix(timeline) : settings

--- timeline/src/main/resources/public/ts/timeline2/app.ts
+++ timeline/src/main/resources/public/ts/timeline2/app.ts
@@ -2,8 +2,10 @@ import angular = require("angular");
 import { OdeModules } from 'ode-ngjs-front';
 import { AppController } from "./controller";
 import * as TimelineApp from './directives/timeline.directive';
+import * as TimelineSettings from './directives/timeline-settings.directive';
 
 
 angular.module("app", [OdeModules.getBase(), OdeModules.getI18n(), OdeModules.getUi(), OdeModules.getWidgets()])
 .controller("appCtrl", ['$rootScope', '$scope', AppController])
-.directive("timeline", TimelineApp.DirectiveFactory);
+.directive("timeline", TimelineApp.DirectiveFactory)
+.directive("timelineSettings", TimelineSettings.DirectiveFactory);
--- timeline/src/main/resources/public/ts/timeline2/controller.ts
+++ timeline/src/main/resources/public/ts/timeline2/controller.ts
@@ -46,67 +46,6 @@ export class AppController {
 };
 
 /*
-export let personalizationController = ng.controller('Personalization', ['$rootScope', '$scope', 'model', ($rootScope, $scope, model) => {
-	$scope.skins = model.skins;
-	$scope.widgets = model.widgets;
-
-	$scope.saveTheme = function(skin, $event){
-		$event.stopPropagation();
-		skin.setForUser();
-		ui.setStyle(skin.path);
-	};
-
-	http().get('/languages').done(function(data){
-		$scope.languages = data;
-    }.bind(this))
-
-	http().get('/userbook/preference/language').done(function(data){
-		try{
-			if(data.preference){
-				$scope.languagePreference = JSON.parse(data.preference)['default-domain']
-			}
-		} catch(e) {
-			$scope.languagePreference = currentLanguage;
-		}
-		if(!$scope.languagePreference)
-			$scope.languagePreference = currentLanguage;
-
-    }.bind(this))
-
-	$scope.saveLang = function(language, $event){
-		$event.stopPropagation();
-		http().putJson('/userbook/preference/language', { 'default-domain': language}).done(function(){
-			location.reload();
-		})
-	};
-
-	$scope.togglePanel = function($event){
-		$scope.showPanel = !$scope.showPanel;
-		$event.stopPropagation();
-	};
-
-	$scope.display = {};
-
-	$scope.showNotifs = function() {
-		$scope.dispaly.showNotifsPanel = true;
-	};
-
-	$scope.hideNotifs = function() {
-		$scope.dispaly.showNotifsPanel = false;
-	};
-
-	$('lightbox[show="display.showNotifsPanel"]').on('click', function(event){
-		event.stopPropagation()
-	});
-
-	$rootScope.$on('close-panel', function(e){
-		$scope.showPanel = false;
-	})
-}]);
-/*
-export let notificationsController = ng.controller('Notifications', ['$scope', 'model', ($scope, model) => {
-
-}]);
 
 export let flashMessagesController = ng.controller('FlashMessages', ['$scope', 'model', ($scope, model) => {
 	$scope.currentLanguage = currentLanguage
