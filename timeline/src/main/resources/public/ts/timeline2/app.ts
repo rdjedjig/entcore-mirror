97becbb89cbdcd29d01692a1e87be95ceb2700ff
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-06-18 10:54:11 +0200

    fix: corrected load order for i18n

--- timeline/src/main/resources/public/ts/timeline2/app.ts
+++ timeline/src/main/resources/public/ts/timeline2/app.ts
@@ -4,10 +4,14 @@ import { AppController } from "./controller";
 import * as TimelineApp from './directives/timeline.directive';
 import * as FlashMsg from './directives/flash-messages.directive';
 import * as TimelineSettings from './directives/timeline-settings.directive';
+import { APP, ConfigurationFrameworkFactory } from "ode-ts-client";
 
 
-angular.module("app", [OdeModules.getBase(), OdeModules.getI18n(), OdeModules.getUi(), OdeModules.getWidgets()])
-.controller("appCtrl", ['$rootScope', '$scope', AppController])
-.directive("timeline", TimelineApp.DirectiveFactory)
-.directive("flashMessages", FlashMsg.DirectiveFactory)
-.directive("timelineSettings", TimelineSettings.DirectiveFactory);
+ConfigurationFrameworkFactory.instance().Platform.apps.initialize(APP.TIMELINE)
+.then( () => {
+    angular.module("app", [OdeModules.getBase(), OdeModules.getI18n(), OdeModules.getUi(), OdeModules.getWidgets()])
+    .controller("appCtrl", ['$rootScope', '$scope', AppController])
+    .directive("timeline", TimelineApp.DirectiveFactory)
+    .directive("flashMessages", FlashMsg.DirectiveFactory)
+    .directive("timelineSettings", TimelineSettings.DirectiveFactory);
+});
