8a73d4246b7bc9251d83778b9d2574858582bde2
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-06-25 09:49:47 +0200

    feat: new timeline settings panel (still needing languages support)

--- timeline/src/main/resources/public/ts/timeline2/directives/timeline-settings.directive.ts
+++ timeline/src/main/resources/public/ts/timeline2/directives/timeline-settings.directive.ts
@@ -1,7 +1,7 @@
 import { IAttributes, IController, IDirective, IScope } from "angular";
 import moment = require("moment");
 import  gsap = require("gsap");
-import { ConfigurationFrameworkFactory, IIdiom, IThemeDesc, IWidget, SessionFrameworkFactory, WidgetFrameworkFactory } from "ode-ts-client";
+import { ConfigurationFrameworkFactory, IIdiom, IThemeDesc, IWidget, SessionFrameworkFactory, WidgetFrameworkFactory, WIDGET_NAME } from "ode-ts-client";
 import * as $ from "jquery";
 import { TimelineController } from "./timeline.directive";
 import { ThemeHelperService } from "ode-ngjs-front";
@@ -39,6 +39,28 @@ export class Controller implements IController {
     	}
 	}
 
+	getWidgetIconClass( widget:IWidget ) {
+		switch( widget.platformConf.name ) {
+			case WIDGET_NAME.BOOKMARK:		return "ic-signets";
+			case WIDGET_NAME.CARNET:		return "ic-carnet-de-bord";
+			case WIDGET_NAME.LAST_INFOS:	return "ic-app actualites";
+			case WIDGET_NAME.MY_APPS:		return "ic-apps";
+			case WIDGET_NAME.RECORD:		return "ic-microphone";
+			case WIDGET_NAME.RSS:			return "ic-rss";
+			// TODO list all classes or use another naming convention in ode-bootstrap
+			// case WIDGET_NAME.AGENDA:		return "ic-";
+			// case WIDGET_NAME.BIRTHDAY:		return "ic-";
+			// case WIDGET_NAME.CALENDAR:		return "ic-";
+			// case WIDGET_NAME.CURSUS:		return "ic-";
+			// case WIDGET_NAME.MAXICOURS:		return "ic-";
+			// case WIDGET_NAME.MOOD:			return "ic-";
+			// case WIDGET_NAME.NOTES:			return "ic-";
+			// case WIDGET_NAME.QWANT:			return "ic-";
+			// case WIDGET_NAME.QWANT_JUNIOR:	return "ic-";
+			default:	return "ic-app";
+		}
+	}
+
 	saveLang(language, $event){
 		ConfigurationFrameworkFactory.instance().User.saveLanguage( language ).then( () => {
 			location.reload();
@@ -54,6 +76,7 @@ interface LocalScope extends IScope {
 /* Directive */
 class Directive implements IDirective<LocalScope,JQLite,IAttributes,IController[]> {
     restrict = 'E';
+	replace = true;
 	template = require("./timeline-settings.directive.html");
     scope = {
     };
